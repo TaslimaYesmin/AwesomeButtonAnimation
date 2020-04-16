6adf3c722d1ba00bc85b667e26ce7cd2f4fd4c42
Taslima Yesmin Orin <you@example.com>
2020-04-16 21:34:41 +0600

    first commit

---
+++ app/src/main/java/com/example/buttonanimation/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.buttonanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      //  button=findViewById(R.id.button);
+      /*  button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Animation myAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bounce);
+                button.startAnimation(myAnim);
+            }
+        });*/
+    }
+}
